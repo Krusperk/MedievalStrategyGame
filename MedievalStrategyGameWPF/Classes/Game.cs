bc681ed29eba7805ae1df5e6827e6da24b2e3b91
Kaspec <kaspecatko@outlook.com>
2023-08-07 21:57:23 +0200

    Game class added (blank)

---
+++ MedievalStrategyGameWPF/Classes/Game.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedievalStrategyGameWPF.Classes
+{
+    internal class Game
+    {
+        List<Player> players;
+
+        public Game()
+        {
+            players= new List<Player>();
+        }
+
+        /// <summary>
+        /// Elemental step of game progression
+        /// </summary>
+        public void NextTurn()
+        {
+
+        }
+
+        /// <summary>
+        /// Player attacking his opponent
+        /// </summary>
+        /// <param name="sender">Who initiated attack</param>
+        /// <returns>Indicate if attack was successfull</returns>
+        public bool Attack(object sender)
+        {
+
+
+            throw new NotImplementedException();
+        }
+    }
+}
