6e0e5f9cfd233ddad0c9c7772bd076fc85aa077b
Kaspec <kaspecatko@outlook.com>
2024-05-28 14:57:48 +0200

    Map and Tile creation

---
+++ MSGLogic/Map.cs
@@ -0,0 +1,12 @@
+﻿namespace MSGLogic
+{
+    public class Map
+    {
+        public Tile[,] Tiles { get; set; }
+
+        public Map(int rows, int columns) 
+        {
+            Tiles = new Tile[rows, columns];
+        }
+    }
+}
---
+++ MSGLogic/Tile.cs
@@ -0,0 +1,12 @@
+﻿namespace MSGLogic
+{
+    public class Tile
+    {
+        public Player Owner { get; set; }
+
+        public Tile(Player owner)
+        {
+            Owner = owner;
+        }
+    }
+}
