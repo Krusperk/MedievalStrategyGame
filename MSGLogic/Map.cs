a9b0a93cd6b4395341c9a95b374bdd05a4e3a1f1
Kaspec <kaspecatko@outlook.com>
2024-08-11 16:27:27 +0200

    Creating Tiles of a Map

--- MSGLogic/Map.cs
+++ MSGLogic/Map.cs
@@ -4,9 +4,24 @@
     {
         public Tile[,] Tiles { get; set; }
 
-        public Map(int rows, int columns) 
+        public Map(int rows, int columns, List<Player> players) 
         {
             Tiles = new Tile[rows, columns];
+
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < columns; c++)
+                {
+                    Player player = (r, c) switch
+                    {
+                        _ when r == 0 && c == columns - 1 => players.First(),
+                        _ when r == rows - 1 && c == 0 => players.Last(),
+                        _ => null
+                    };
+
+                    Tiles[r, c] = new Tile(player);
+                }
+            }
         }
     }
 }
